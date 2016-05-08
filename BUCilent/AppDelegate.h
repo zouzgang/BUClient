f921fea60fb92486c34f169e0a7b21cea9d2ed95
zouzhigang <zouzhigang@loopeer.com>
2016-05-08 23:14:27 +0800

    Initial Commit

---
+++ BUCilent/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BUCilent
+//
+//  Created by dito on 16/5/8.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BUCilent/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BUCilent
+//
+//  Created by dito on 16/5/8.
+//  Copyright © 2016年 zouzhigang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
